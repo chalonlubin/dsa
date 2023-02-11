a115576bb648fd2fcaf95426dd407892a618292d
Chalon Lubin <chalonlubin1@gmail.com>
2023-02-11 00:52:17 -0800

    p2

--- everyThird.js
+++ everyThird.js
@@ -1,3 +1,14 @@
-function everyThird() {}
+function everyThird(arr, idx=2, result=[]) {
+  if (idx >= arr.length) return result;
+  result.push(arr[idx]);
+  everyThird(arr, idx+3, result)
+  return result;
+
+}
+
+let arr = [10, 20, 30, 40, 50, 60, 70]
+
+
+console.log("🚀 ~ file: everyThird.js:9 ~ everyThird(arr)", everyThird(arr))
 
 module.exports = everyThird;
